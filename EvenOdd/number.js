3551139541266bdc349e28cffe74b08ebbdf1e25
Wandati <Petersimon801@gmail.com>
2023-06-07 12:55:32 +0300

    OddEven

---
+++ EvenOdd/number.js
@@ -0,0 +1,10 @@
+function IsCheck(){
+    const Num = parseInt(prompt(" Enter Your number? "))
+    if (Num % 2 == 0){
+    console.log("The number is Even!")
+    }else{
+    console.log("The number is Odd!")
+    
+    }
+}
+IsCheck()
